5431697dc43ccaa8e3ba8475a57cb0e9c8905e59
Sardorbek1706 <sardorbektursunboyev06@gmail.com>
2025-11-20 19:51:25 +0500

    Delete middleware directory

--- middleware/roleGuard.js
+++
@@ -1,9 +0,0 @@
-/*
-  roleGuard: permit(...roles) => middleware
-*/
-module.exports = function permit(...allowed){
-  return (req, res, next) => {
-    // TODO: check req.user.role
-    next();
-  };
-};
